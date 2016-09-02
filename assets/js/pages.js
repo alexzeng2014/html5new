69c6dcd5e95e32d5504bea6396f4b1a783ce4da1
alexzeng2014 <alexzeng@msn.com>
2016-09-03 00:30:05 +0800

    pages.js失败

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -5,16 +5,32 @@
             firstText: '第一页',
             lastText: '最后一页'
         };
-        var options = $.extend(defaults, opts)
-        return this.each(function () {
-            $(this).append("<ul class='pagination'></ul>");
-            var $Element = $(this).find(".pagination");
-            $Element.append("<li><a href='#'>" + options.firstText + "</a></li>");
-            for (var i = 0; i < options.total; i++) {
-                $Element.append("<li><a href='#'>" + (i + 1) + "</a></li>")
-            }
-            $Element.append("<li><a href='#'>" + options.lastText + "</a></li>");
-
+        var options = $.extend(defaults, opts);
+         return this.each(function () {
+            var $Element = $(this);
+            forpage(1,$Element);
+            var $li_a_click=$Element.find("ul.pagination li:not(:active) a");
+             $li_a_click.on('click',function(){
+                 forpage($(this).text(),$Element);
+             });
         });
+         function forpage(index_page,$div){
+             $div.empty();
+            $div.append("<ul class='pagination'></ul>");
+            var $this_ul=$div.find(".pagination");
+             $this_ul.append("<li><a href='#'>" + options.firstText + "</a></li>");
+            for (var i = 0; i < options.total; i++) {
+                if (index_page==i+1){
+                     $this_ul.append("<li class='active'><a href='#'>" + (i + 1) + "<span class='sr-only'>(current)</span></a></li>");
+                }else{
+                 $this_ul.append("<li><a href='#'>" + (i + 1) + "</a></li>");
+                }
+            }
+            $this_ul.append("<li><a href='#'>" + options.lastText + "</a></li>");
+        }
+
+
+
+
     };
 })(jQuery);
