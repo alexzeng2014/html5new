7407f77610eaeab3c47038f156746d71e7dba7bf
alexzeng2014 <mianhu@gmail.com>
2016-09-03 19:46:51 +0800

    修改pages.js

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -6,31 +6,46 @@
             lastText: '最后一页'
         };
         var options = $.extend(defaults, opts);
-         return this.each(function () {
+        return this.each(function () {
             var $Element = $(this);
-            forpage(1,$Element);
-            var $li_a_click=$Element.find("ul.pagination li:not(:active) a");
-             $li_a_click.on('click',function(){
-                 forpage($(this).text(),$Element);
-             });
+            forpage(1, $Element);
         });
-         function forpage(index_page,$div){
-             $div.empty();
+
+
+        function forpage(index_page, $div) {
+            $div.empty();
             $div.append("<ul class='pagination'></ul>");
-            var $this_ul=$div.find(".pagination");
-             $this_ul.append("<li><a href='#'>" + options.firstText + "</a></li>");
-            for (var i = 0; i < options.total; i++) {
-                if (index_page==i+1){
-                     $this_ul.append("<li class='active'><a href='#'>" + (i + 1) + "<span class='sr-only'>(current)</span></a></li>");
-                }else{
-                 $this_ul.append("<li><a href='#'>" + (i + 1) + "</a></li>");
+            var $this_ul = $div.find(".pagination");
+            var intbegin,intend;
+            if (options.total<10){
+                intbegin=0;
+                intend=options.total;
+            }
+            else if(index_page<6 && options.total>=10){
+                 intbegin=0;
+                intend=options.total-10;
+            }
+            else if (index_page>options.total-10  && options.total>=10){
+                intbegin=options.total-10;
+                 intend=options.total-1;
+            }else{
+                intbegin=index_page-5;
+                intend=index_page+3;
+            }
+            $this_ul.append("<li><a href='#' value='1'>" + options.firstText + "</a></li>");
+            for (var i = intbegin; i < intend; i++) {
+                if (index_page == i + 1) {
+                    $this_ul.append("<li class='active'><a href='#'>" + (i + 1) + "<span class='sr-only'>(current)</span></a></li>");
+                } else {
+                    $this_ul.append("<li><a href='#' value='" + (i + 1) + "'>" + (i + 1) + "</a></li>");
                 }
             }
-            $this_ul.append("<li><a href='#'>" + options.lastText + "</a></li>");
+            $this_ul.append("<li><a href='#' value='" + options.total + "'>" + options.lastText + "</a></li>");
+            $this_ulliclick = $this_ul.find("li:not(.active) a");
+            $this_ulliclick.on('click', function () {
+                var id = $(this).attr("value");
+                forpage(id, $div);
+            });
         }
-
-
-
-
     };
 })(jQuery);
